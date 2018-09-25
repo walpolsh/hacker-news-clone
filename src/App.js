3306a3aa60885a81509ebc6ecab7df6dc0dc69ef
ProgRockPaul <twentytwentyk@live.com>
2018-09-24 21:21:26 -0400

    first

--- src/App.js
+++ src/App.js
@@ -7,16 +7,132 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
-const url = `${PATH_BASE}${PATH_BASE}${PARAM_SEARCH}${DEFAULT_QUERY}`
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+    }
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id; 
+    
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+
   render() {
+    console.log(this.state)
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
     return (
-      <div className="App">
-       
+      <div className="page">
+        <div className="interactions">        
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+              Search
+          </Search>
+
+          <Table 
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        </div>
       </div>
     );
   }
 }
+
+const Search = ({ value, onChange, children }) => 
+    <form>
+      {children}<input 
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </form>
+
+const largeColumn = {
+  width: '40%',
+}
+const mediumColumn = {
+  width: '30%',
+}
+const smallColumn = {
+  width: '10%',
+}
+
+const Table = ({ list, pattern, onDismiss }) =>
+      <div className='table'>
+        {list.filter(isSearched(pattern)).map(item => 
+          <div key={item.objectID} className='table-row'> 
+            <span style={largeColumn}>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span style={mediumColumn}>
+              {item.author}
+            </span>
+            <span style={smallColumn}>
+              {item.num_comments}
+            </span>
+            <span style={smallColumn}>
+              {item.points}
+            </span>
+            <span style={smallColumn}>
+              <Button 
+                className='button-inline'
+                onClick={() => onDismiss(item.objectID)}
+              >
+                Dismiss
+              </Button>
+            </span>
+          </div>
+          )}
+      </div>
+
+const Button = ({onClick, className = '', children}) => 
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+       {children}
+      </button>
+
 
 export default App;
